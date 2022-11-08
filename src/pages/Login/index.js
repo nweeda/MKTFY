dee16fc5507542060ac7d72f7d1c3e86aef15a7e
Nick <nicholas.weeda@gmail.com>
2022-11-08 13:04:50 -0800

    more progress on login styling (centered vertically)

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import BasicButton from '../../components/buttons/BasicButton';
-import './login.css';
-import Logo from '../../assets/MKTFYlogo.png';
-//import Column from 'antd/lib/table/Column';
-import Page from '../muiTesting/grid.js';
+import React from "react";
+import BasicButton from "../../components/buttons/BasicButton";
+import "./login.css";
+import Logo from "../../assets/MKTFYlogo.png";
+import Page from "../muiTesting/grid.js";
 
-function Login(){
-    return (
-        <div style={{display:"flex", flexDirection: "column", alignItems: "center"}}>
-                <img src={Logo} style={{width: '235px'}}/>
-                <BasicButton style={{display:"flex", }}>Login</BasicButton>
-                <BasicButton>Create Account</BasicButton>
-                <Page>TEST</Page>
-        </div>
-    );  
+function Login() {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img src={Logo} style={{ width: "235px", height: "88px" }} />
+      <BasicButton style={{ display: "flex" }}>Login</BasicButton>
+      <BasicButton>Create Account</BasicButton>
+      <p>Find out more about us! Visit our website!</p>
+    </div>
+  );
 }
 
 export default Login;
