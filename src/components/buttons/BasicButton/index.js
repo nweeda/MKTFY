a3cfbd3f81a1257937f5ca345b133ffac221be1b
Nick <nicholas.weeda@gmail.com>
2022-11-08 17:42:07 -0800

    corrected button margins and updated layout

--- src/components/buttons/BasicButton/index.js
+++ src/components/buttons/BasicButton/index.js
@@ -1,23 +1,33 @@
-import Link from 'antd/lib/typography/Link';
-import React from 'react';
-import './basicButton.css'
+import Link from "antd/lib/typography/Link";
+import React from "react";
+import "./basicButton.css";
 
-function BasicButton(props){
-//if props.color === 
-//else if props.color ===
-let buttonStyle={}
-//two ways:
-if(props.color==='purple'){
-    buttonStyle.backgroundColor='purple'
-}
+function BasicButton(props) {
+  //if props.color ===
+  //else if props.color ===
+  let buttonStyle = {
+    // ...props.styles
+  };
+  //two ways:
+  if (props.color === "purple") {
+    buttonStyle.backgroundColor = "purple";
+  }
 
-let buttonClass='myClass'
-if(props.color==='purple'){
-    buttonClass=buttonClass+" purpleButton"
-}
-    return (
-        <button style={buttonStyle} className={buttonClass}>{props.children}</button>
-    );  
+  let buttonClass = "bigBut";
+  if (props.color === "purple") {
+    buttonClass = buttonClass + " purpleButton";
+  }
+
+  console.log("buttonStyle: ", buttonStyle);
+  console.log("buttonClass: ", buttonClass);
+  return (
+    <button
+      style={buttonStyle}
+      className={buttonClass + (props.classNames ? " " + props.classNames : "")}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default BasicButton;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,12 +6,21 @@ import Page from "../muiTesting/grid.js";
 
 function Login() {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <img src={Logo} style={{ width: "235px", height: "88px" }} />
-      <BasicButton style={{ display: "flex" }}>Login</BasicButton>
-      <BasicButton>Create Account</BasicButton>
+    <div className="bg-img">
+      <div className="login-container">
+        <img
+          src={Logo}
+          style={{ width: "235px", height: "88px", marginBottom: "29px" }}
+          alt="Logo"
+        />
+        <BasicButton style={{ display: "flex" }}>Login</BasicButton>
+        <BasicButton
+          // styles={{ marginTop: "18px" }}
+          classNames="marginTopBut"
+        >
+          Create Account
+        </BasicButton>
+      </div>
       <p>Find out more about us! Visit our website!</p>
     </div>
   );
